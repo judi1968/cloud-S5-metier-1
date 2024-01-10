f7f371d0a3f159f581bc1734eeaea18de87bef2c
jeanmirli <jeanmirlin.r@gmail.com>
2024-01-10 06:41:55 +0300

    Update LoginController.java

--- src/main/java/projetS5/cloud/projetCloud/Controllers/LoginController.java
+++ src/main/java/projetS5/cloud/projetCloud/Controllers/LoginController.java
@@ -2,6 +2,7 @@ package projetS5.cloud.projetCloud.Controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import projetS5.cloud.projetCloud.Context.PgsqlContext;
@@ -15,7 +16,7 @@ import java.sql.SQLException;
 @RequestMapping("login")
 public class LoginController {
 
-    @GetMapping("admin")
+    @PostMapping("admin")
     public Bag Login(HttpServletRequest request) throws SQLException {
         String exception = null;
         Object object = null;
@@ -24,11 +25,13 @@ public class LoginController {
             connection = PgsqlContext.connect();
             String email = request.getParameter("name");
             String password = request.getParameter("password");
+
             String adminId = Admin.login(connection, email, password);
-            object = adminId;
+            object = password;
+            exception = email;
         }
         catch (Exception e) {
-            exception = e.getMessage();
+            //exception = e.getMessage();
         }
         finally {
             if (!connection.isClosed() && connection != null) connection.close();
