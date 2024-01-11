fec76ed48734f5ca8caea7217735b10bba264e6b
Judi <judiherinirina04@gmail.com>
2024-01-11 18:10:31 +0300

    Create ConfigReact.java

---
+++ src/main/java/projetS5/cloud/projetCloud/configuration/ConfigReact.java
@@ -0,0 +1,26 @@
+package projetS5.cloud.projetCloud.configuration;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class ConfigReact {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*"); // Remplacez par l'URL de votre application React
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
+
