e44c6b9a7bebbe180238f2e0e8e18f48fe5c9e01
Judi <judiherinirina04@gmail.com>
2024-01-16 23:55:42 +0300

    Update LoginController.java

--- src/main/java/projetS5/cloud/projetCloud/Controllers/LoginController.java
+++ src/main/java/projetS5/cloud/projetCloud/Controllers/LoginController.java
@@ -51,29 +51,31 @@ public class LoginController {
     @PostMapping("/log_admin_traitement")
     public Map<String, Object> initializer(@RequestBody Map<String, Object> requestBody) {
         Map<String, Object> resultat = new HashMap<>();
-        int status = 200;
-        String titre = "";
+        int status = 0;
+        String titre = null;
         String message = null;
         Map<String, Object> data = new HashMap<>();
         Vector<String> donnes = new Vector<>();
-
+    
         try {
             String name = (String) requestBody.get("name");
             donnes.add(name);
             donnes.add((String) requestBody.get("password"));
-            if (name.compareToIgnoreCase("judi")!=0) {
-                throw new Exception("Nom non valider");
-            }else{
-                status = 200;
-                titre = "S'authentification VaikaNet";
-                message = "Vous êtes le bienvenu sur le projet";
-                // Générer un token JWT
-                String token = Jwts.builder()
-                .setSubject(name)
-                .signWith(SignatureAlgorithm.HS256, "PER_AT0001") // Remplacez "votre-cle-secrete" par une clé secrète réelle : eto zao izy idPersonne
-                .compact();
-                data.put("token", token);
+    
+            if (name.compareToIgnoreCase("judi") != 0) {
+                throw new Exception("Nom non valide");
             }
+    
+            // Générer un token JWT
+            String token = Jwts.builder()
+                    .setSubject(name)
+                    .signWith(SignatureAlgorithm.HS256, "votre-cle-secrete") // Remplacez "votre-cle-secrete" par une clé secrète réelle
+                    .compact();
+    
+            status = 200;
+            titre = "S'authentification VaikaNet";
+            message = "Vous êtes le bienvenu sur le projet";
+            data.put("token", token);
         } catch (Exception e) {
             status = 500;
             titre = "Authentification a échoué";
