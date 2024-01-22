2e56566a77b346a84f69119960423307519ec44d
Judi <121221532+JudiNah@users.noreply.github.com>
2024-01-22 13:57:57 +0300

    Update data.sql

--- src/main/java/projetS5/cloud/projetCloud/script/data.sql
+++ src/main/java/projetS5/cloud/projetCloud/script/data.sql
@@ -23,14 +23,14 @@ INSERT INTO marque_voiture (nom, description, date_creation) VALUES
 INSERT INTO type_carburant_voiture (nom) VALUES
 ('Essence'),
 ('Diesel'),
-('Électrique'),
-('Hybride essence/électrique'),
-('Hybride diesel/électrique');
+('Electrique'),
+('Hybride essence/electrique'),
+('Hybride diesel/electrique');
 
 INSERT INTO transmission_voiture (nom) VALUES
 ('Automatique'),
 ('Manuelle'),
-('CVT (Transmission à variation continue)'),
+('Transmission variation continu'),
 ('Semi-automatique'),
 ('4 roues motrices');
 
@@ -38,9 +38,9 @@ INSERT INTO transmission_voiture (nom) VALUES
 INSERT INTO freignage_voiture (nom) VALUES
 ('Freins à disque'),
 ('Freins à tambour'),
-('Système de freinage antiblocage (ABS)'),
-('Freinage d urgence assisté'),
-('Frein de stationnement électronique');
+('Systeme freinage antiblocage'),
+('Freinage d urgence assiste'),
+('Frein stationnement electro');
 
 INSERT INTO equipement_interne (nom) VALUES
 ('Système de navigation GPS'),
@@ -59,17 +59,17 @@ INSERT INTO voiture (annee_fabrication, couleur, consommation, categorie_voiture
 
 -- Insertion de données dans la table voiture_equipement_interne
 INSERT INTO voiture_equipement_interne (voiture_id, equipement_interne_id) VALUES
-('VOIT0001', 'EQP_IN0001'), -- Première voiture avec système de navigation GPS
-('VOIT0002', 'EQP_IN0002'), -- Deuxième voiture avec climatisation automatique
-('VOIT0003', 'EQP_IN0003'), -- Troisième voiture avec sièges en cuir chauffants
-('VOIT0004', 'EQP_IN0004'), -- Quatrième voiture avec toit ouvrant panoramique
-('VOIT0005', 'EQP_IN0005'); -- Cinquième voiture avec système audio haut de gamme
+('VOIT0006', 'EQP_IN0001'), -- Première voiture avec système de navigation GPS
+('VOIT0007', 'EQP_IN0002'), -- Deuxième voiture avec climatisation automatique
+('VOIT0008', 'EQP_IN0003'), -- Troisième voiture avec sièges en cuir chauffants
+('VOIT0009', 'EQP_IN0004'), -- Quatrième voiture avec toit ouvrant panoramique
+('VOIT0010', 'EQP_IN0005'); -- Cinquième voiture avec système audio haut de gamme
 
 
 -- Insertion de données dans la table voiture_prix
 INSERT INTO voiture_prix (prix, date, voiture_id) VALUES
-(25000.00, '2022-01-01', 'VOIT0001'),
-(28000.00, '2022-02-15', 'VOIT0002'),
-(22000.00, '2022-03-20', 'VOIT0003'),
-(30000.00, '2022-04-10', 'VOIT0004'),
-(35000.00, '2022-05-05', 'VOIT0005');
+(25000.00, '2022-01-01', 'VOIT0006'),
+(28000.00, '2022-02-15', 'VOIT0007'),
+(22000.00, '2022-03-20', 'VOIT0008'),
+(30000.00, '2022-04-10', 'VOIT0009'),
+(35000.00, '2022-05-05', 'VOIT0010');
